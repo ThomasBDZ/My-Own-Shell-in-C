32125b0428610e35128cadc77747bd4cc8399571
BOUGUET Theo <Theo.Bouguet@telecomnancy.eu>
2021-10-27 13:08:21 +0200

    [Modif] Utilisation de strdup pour copier les arguments

--- tesh.c
+++ tesh.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "fcntl.h"
 #include <string.h>
+//#include <unistd.h>
+//#include <sys/wait.h>
+
 
 
 int main(int argc, char const *argv[])
@@ -11,38 +14,25 @@ int main(int argc, char const *argv[])
 
         char commande[200];
         fgets(commande,200,stdin);
-        const char *sep = " \n";
-        char help[200];
-        strcpy(help,commande);
-
-        char * strtoken = strtok(commande,sep);
-        int i =0 ;
+        const char *sep = " \n"; 
+        char *args[20];
+        
+        char *strtoken = strtok(commande,sep);
+        int i =0;
         
         while(strtoken != NULL){
+            args[i] = strdup(strtoken);
             strtoken = strtok(NULL,sep);
             i++;
         } 
-        
-        char **args = calloc(i,sizeof(char*));
-        for(int j=0;j<i;j++){
-            args[j] = calloc(1,sizeof(char)*20);
-        }
-        strtoken = strtok(help,sep);
-        int k =0;
-        
-        while(strtoken != NULL){
-            strcpy(args[k],strtoken);
-            strtoken = strtok(NULL,sep);
-            k++;
-        } 
+        args[i+1] = NULL;
 
         
         if(i <1 ){
             printf("Il manque au moins 1 argument\n");
             exit(1);
         }
-        
-        if(fork() == 0){
+        else if(fork() == 0){
             execvp(args[0],args);
             perror("Erreur");
             exit(0);
@@ -51,11 +41,10 @@ int main(int argc, char const *argv[])
             wait(NULL);
         }
         
-
+        
         for(int j=0;j<i;j++){
             free(args[j]);
         }
-        free(args);
 
     }
 
