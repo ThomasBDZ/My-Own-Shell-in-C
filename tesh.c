35bff4d197015161fa769df2b8e7dffbb7de7da3
BOUGUET Theo <Theo.Bouguet@telecomnancy.eu>
2021-11-04 17:24:36 +0100

    [Ajout/Modif] Deplacement de l'affichage du prompt dans une fonction + enchainement de commande separe par ';'

--- tesh.c
+++ tesh.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "fcntl.h"
 #include <string.h>
 #include <unistd.h>
@@ -11,73 +10,88 @@
 #define MAX_HOSTNAMEID_LENGTH 256
 #define MAX_CurrentDir_LENGTH 256
 
-int main(int argc, char const *argv[])
-{
+int executeCMD(char **cmdArgs, int i){
 
-    char username[MAX_USERID_LENGTH];
-        char hostname[MAX_USERID_LENGTH];
-        char repcourant[MAX_CurrentDir_LENGTH];
-        cuserid(username);
-        /*getlogin_r(username, MAX_USERID_LENGTH);*/
-        gethostname(hostname, MAX_HOSTNAMEID_LENGTH);
-        getcwd(repcourant,MAX_CurrentDir_LENGTH);
-
-    char entree[200];
-    char commande[200];
-
-    printf("\n%s@%s:%s$ ",username,hostname,repcourant);
-
-    while(1 && fgets(entree,200,stdin) != NULL){
-
-        sscanf(entree,"%[^\n]",commande);
-        //fgets(commande,200,stdin);
-        const char *sep = " "; 
-        char *args[20];
-        
-        char *strtoken = strtok(commande,sep);  
-        int i =0;
-        
-        while(strtoken != NULL){
-            //printf("%s\n\n",strtoken);
-            args[i] = strtoken;
-            strtoken = strtok(NULL,sep);
-            i++;
-        } 
-        
-        args[i] = NULL;
-        
-        if(i <1 ){
+    cmdArgs[i] = NULL;
+    if(i <1 ){
             printf("Il manque au moins 1 argument\n");
             exit(1);
         }
-        if(strcmp(args[0],"cd")==0){
+        if(strcmp(cmdArgs[0],"cd")==0){
             
-            int r = chdir(args[1]);
+            int r = chdir(cmdArgs[1]);
             if(r == -1){
             perror("Error");
             }
         }
         else if(fork() == 0){
-            execvp(args[0],args);
-            perror("Erreur");
+            execvp(cmdArgs[0],cmdArgs);
+            perror("Error");
             exit(0);
         }
         else{
            wait(NULL);
-
         }
-
-        char username[MAX_USERID_LENGTH];
-        char hostname[MAX_USERID_LENGTH];
-        char repcourant[MAX_CurrentDir_LENGTH];
-        cuserid(username);
-        /*getlogin_r(username, MAX_USERID_LENGTH);*/
-        gethostname(hostname, MAX_HOSTNAMEID_LENGTH);
-        getcwd(repcourant,MAX_CurrentDir_LENGTH);
-        printf("\n%s@%s:%s$ ",username,hostname,repcourant);
-              
-    }
     
     return 0;
 }
 
+void afficherPrompt(){
+    char username[MAX_USERID_LENGTH];
+    char hostname[MAX_USERID_LENGTH];
+    char repcourant[MAX_CurrentDir_LENGTH];
+    cuserid(username);
+    /*getlogin_r(username, MAX_USERID_LENGTH);*/
+    gethostname(hostname, MAX_HOSTNAMEID_LENGTH);
+    getcwd(repcourant,MAX_CurrentDir_LENGTH);
+    printf("\n%s@%s:%s$ ",username,hostname,repcourant);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    char entree[200];
+    char commande[200];
+
+    afficherPrompt();
+
+    while(1 == 1 && fgets(entree,200,stdin) != NULL){
+
+        sscanf(entree,"%[^\n]",commande);
+        
+        const char *sep = " "; 
+        char *args[20];
+        
+        char *strtoken = strtok(commande,sep);  
+        int i =0;
+        int r;
+
+        while(strtoken != NULL){
+
+            if(strcmp(strtoken,";") != 0){
+                args[i] = strtoken;
+                i++;
+                
+            }else if(strcmp(strtoken,";") == 0){
+                
+                r = executeCMD(args,i);
+                int j =0;
+                while(args[j+1] != NULL){
+                    args[j] = NULL;
+                    j++;
+                }
+                i = 0;
+
+            }
+            strtoken = strtok(NULL,sep);
+
+        } 
+        
+        executeCMD(args,i);
+
+        afficherPrompt();
+              
+    }   
+    return 0;
+}
